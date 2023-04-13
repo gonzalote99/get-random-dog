9ba34c48dcd9f83bf87c3dd9db7e8cbbc71b4686
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-04-13 15:08:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# get-random-dog
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-3jhp7t)
---
+++ script.js
@@ -0,0 +1,15 @@
+const image = document.getElementById('dogImage');
+
+const fetchImage = () => {
+  fetch("https://dog.ceo/api/breeds/image/random").then(
+    response => response.json()
+
+  ).then(
+    json => image.innerHTML = `<img src="${json.message}" style="height: 30%; width: 40%">`
+  )
+}
+
+const btn = document.getElementById("dogButton")
+btn.onclick = () => {
+  fetchImage();
+}
